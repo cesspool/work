04284a03b7575780af977ba9b3c080f1a3206999
PaulCardLord <paulkapay@gmail.com>
2018-03-03 15:12:56 +0300

    all works

---
+++ src/main/java/web/Pages.java
@@ -0,0 +1,74 @@
+package web;
+
+public class Pages {
+    /**
+     * Application main page (all can see)
+     */
+    public static final String MAIN = "index";
+
+    /**
+     * page shows current and next rate(tariff) (all can see)
+     */
+    public static final String RATE = "rate";
+
+    /**
+     * main page for admin for adding new node (admin can see)
+     */
+    public static final String ADMIN = "adminMain";
+
+    /**
+     * page shows information about the customer(from customer's personal area)
+     */
+    public static final String CONTACT = "contact";
+
+    /**
+     * page shows completed orders(from customer's personal area)
+     */
+    public static final String HISTORY = "history";
+
+    /**
+     * authorization page
+     */
+    public static final String LOGIN = "login";
+
+    /**
+     * page for adding new boxing (admin can see)
+     */
+    public static final String BOXING = "newBoxing";
+
+    /**
+     * page for adding new rate(tariff) (admin can see)
+     */
+    public static final String NEWRATE = "newRate";
+
+    /**
+     * main page for customer's personal area that shows orders in progress
+     */
+    public static final String PERSON = "personal";
+
+    /**
+     * page for calculating new order (customer can see)
+     */
+    public static final String COST = "personCost";
+
+    /**
+     * page that shows result of transportation's calculating (customer can see)
+     */
+    public static final String RESULT = "resultCost";
+
+    /**
+     * page for sending new order(customer can see)
+     */
+    public static final String ORDER = "personOrder";
+
+    /**
+     * registration page
+     */
+    public static final String REGISTRATION = "registration";
+
+    /**
+     * page for changing status of orders (manager can see)
+     */
+    public static final String STATUS = "statusOrder";
+
+}
